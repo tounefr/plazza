7bab13f93705ec2bba29af633eae84ff4f2d0f5b
Thomas HENON <thomas.henon@epitech.eu>
2017-04-28 14:34:11 +0200

    wip

--- common/Logger.cpp
+++ common/Logger.cpp
@@ -36,20 +36,21 @@ void Logger::print(LogType type, const std::string &where, const std::string &wh
         time_t now = time(0);
         tm *ltm = localtime(&now);
 
-    std::stringstream ss;
-    ss << std::this_thread::get_id();
-    uint64_t thread_id = std::stoull(ss.str());
+        std::stringstream ss;
+        ss << std::this_thread::get_id();
+        uint64_t thread_id = std::stoull(ss.str());
 
-    std::cout << ltm->tm_mday << "/"
-              << 1 + ltm->tm_mon << "/"
-              << 1900 + ltm->tm_year << " "
-              << 1 + ltm->tm_hour << ":"
-              << 1 + ltm->tm_min << ":"
-              << 1 + ltm->tm_sec << " "
-              << " [" << Types[type] << "] "
-              << "["+ std::to_string(thread_id) +"] "
-              << "[" << where << "] : "
-              << what << std::endl;
+        std::cout << ltm->tm_mday << "/"
+                  << 1 + ltm->tm_mon << "/"
+                  << 1900 + ltm->tm_year << " "
+                  << 1 + ltm->tm_hour << ":"
+                  << 1 + ltm->tm_min << ":"
+                  << 1 + ltm->tm_sec << " "
+                  << " [" << Types[type] << "] "
+                  << "[" + std::to_string(thread_id) + "] "
+                  << "[" << where << "] : "
+                  << what << std::endl;
+    }
 
     /*
     if (type == ERROR)
